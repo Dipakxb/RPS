f75af7e1ae31da1e55e5c6bc53391b4c64c73210
Dipak <dipakxb@gmail.com>
2025-12-22 19:50:23 +0530

    Add basic ui structure
    add basic buttons and div for displaying
    and interacting with the game

--- script.js
+++ script.js
@@ -7,30 +7,30 @@ function getComputerChoice() {
     const randomNumber = Math.floor(Math.random() * 100) + 1;
     let choice;
     if(randomNumber <= 33 && randomNumber >= 0) {
-        choice = "paper"
+        choice = "paper";
     }
     if(randomNumber <= 66 && randomNumber > 33){
-        choice = "scissors"
+        choice = "scissors";
     }
     if(randomNumber <= 99 && randomNumber > 66) {
-        choice = "rock"
+        choice = "rock";
     }
-    return choice
+    return choice;
 }
 
 // get human choice get user choice that should be "rock" "paper" "scissors"
-function getHumanChoice() {
+//could have been a sleeker design
 
-    // could have been a sleeker design
-    /* let choice = ""
+/* function getHumanChoice() {
+    let choice = ""
     do{
         choice = (prompt(`Choose from "paper" "scissors" "rock" \nlet's play: `))
         console.log(choice)
     }
-    while(choice !== "rock" && choice !== "paper" && choice !== "scissors")  */
+    while(choice !== "rock" && choice !== "paper" && choice !== "scissors") 
 
     return prompt(`Choose from "paper" "scissors" "rock" \nlet's play: `)
-}
+} */
 
 // plays a round with user and computer choice as arguments declare a winner and increment the score of winner
 
@@ -38,50 +38,71 @@ function playRound(userChoice, compChoice) {
     let value = "Enter a valid Choice";
     
     if(userChoice === compChoice){
-        value = "It's a Tie play again"
+        value = "It's a Tie play again";
     }
     if(userChoice === "paper" && compChoice === "scissors"){
-        value = "You Lose! scissors Beats paper"
-        computerScore++
+        value = "You Lose! scissors Beats paper";
+        computerScore++;
     }
     if(userChoice === "paper" && compChoice === "rock"){
-        value = "You Won! paper Beats rock"
-        userScore++
+        value = "You Won! paper Beats rock";
+        userScore++;
     }
     if(userChoice === "scissors" && compChoice === "paper"){
-        value = "You Won! scissors Beats paper"
-        userScore++
+        value = "You Won! scissors Beats paper";
+        userScore++;
     }
     if(userChoice === "scissors" && compChoice === "rock"){
-        value = "You Lose! rock Beats scissors"
-        computerScore++
+        value = "You Lose! rock Beats scissors";
+        computerScore++;
     }
     if(userChoice === "rock" && compChoice === "paper"){
-        value = "You Lose! paper Beats rock"
-        computerScore++
+        value = "You Lose! paper Beats rock";
+        computerScore++;
     }
     if(userChoice === "rock" && compChoice === "scissors"){
-        value = "You Won! rock Beats scissors"
-        userScore++
+        value = "You Won! rock Beats scissors";
+        userScore++;
     }
     return value;
 }
 
 // play five rounds keep track of the score and declare a winner
 
-function playGame() {
+
+// playGame();
+
+const div = document.querySelector('.buttons');
+const resultDiv = document.querySelector('.resultDiv');
+
+function playGame(humanChoice) {
+    const score = document.createElement('p');
+    const current = document.createElement('p');
+    const result = document.createElement('p');
+    resultDiv.appendChild(score);
+    resultDiv.appendChild(current);
+    resultDiv.appendChild(result);
+
     let value = "";
     for(let x = 0; x < 5; x++){
-        console.log(playRound(getHumanChoice().toLowerCase(), getComputerChoice()))
-        console.log(`Computer: ${computerScore} User: ${userScore}`)
+        current.innerText = playRound(humanChoice, getComputerChoice());
+        score.innerText = `Computer: ${computerScore} User: ${userScore}`;
+    
+        if(x >= 5){
+            if(userScore > computerScore) {
+                value = "You have won the Game!!";
+                break;
+            }
+            else{
+                value = "better Luck next Time!";
+                break;
+            }
+        }
     }
-    if(userScore > computerScore) {
-        value = "You have won the Game!!"
-    }
-    else{
-        value = "better Luck next Time!"
-    }
-    return console.log(value)
+    result.innerText = value;
 }
 
-playGame();
+div.addEventListener('click', (e)=> {
+    const selection = e.target.getAttribute('class');
+    playGame(selection);
+})
